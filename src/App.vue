2afb811f63025b2017e890e83d89bcb5f1ad34d1
songstar <1043328710@qq.com>
2019-08-16 16:24:37 +0800

    新增缓存主题过渡加载

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,15 @@
   </div>
 </template>
 <script>
+import { Loading } from 'element-ui'
 import { handleTheme } from '@/utils/theme'
 export default {
-  created () {
-    this.$loading.service()
-    handleTheme(this.$store.state.app.theme)
+  async created () {
+    let $loading = Loading.service()
+    await handleTheme(this.$store.state.app.theme)
+    this.$nextTick(() => { // 以服务的方式调用的 Loading 需要异步关闭
+      $loading.close()
+    })
   }
 }
 </script>
